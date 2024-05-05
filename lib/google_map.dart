a023b71dba8482c7391736cb89419274d207340a
qjvk2880 <qjvk2880@gmail.com>
2024-05-06 01:01:59 +0900

    feat: 이동 시 픽셀 색 변화 기능 추가

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -25,9 +25,11 @@ class _MapTestState extends State<MapTest> {
 
   Map<String, Polygon> polygons = {};
 
+  late String currentPolygonId;
+
   void initPolygons() {
-    double initLat = 37.508600;
-    double initLng = 127.041452;
+    double initLat = 37.603832;
+    double initLng = 126.921212;
 
     double tenMeterInLat = 1 / 3600;
     double tenMeterInLng = 1 / 3600;
@@ -36,8 +38,8 @@ class _MapTestState extends State<MapTest> {
       for (var j = 0; j < 25; j++) {
         List<LatLng> latLngList = [];
 
-        latLngList.add(LatLng(initLat + i * tenMeterInLat, initLng + j * tenMeterInLng));
-        latLngList.add(LatLng(initLat + i * tenMeterInLat, initLng + ((j + 1) * tenMeterInLng)));
+        latLngList.add(LatLng(initLat - i * tenMeterInLat, initLng + j * tenMeterInLng));
+        latLngList.add(LatLng(initLat - i * tenMeterInLat, initLng + ((j + 1) * tenMeterInLng)));
         latLngList.add(LatLng(initLat - ((i + 1) * tenMeterInLat), initLng + ((j + 1) * tenMeterInLng)));
         latLngList.add(LatLng(initLat - ((i + 1) * tenMeterInLat), initLng + j * tenMeterInLng));
 
@@ -46,19 +48,19 @@ class _MapTestState extends State<MapTest> {
             points: latLngList,
             fillColor: Colors.transparent,
             strokeColor: Colors.red,
-            strokeWidth: 1,
-        );
-
-        // polygonData?.add(latLngList);
+            strokeWidth: 1
+        );// polygonData?.add(latLngList);
       }
     }
+
   }
 
   @override
   void initState() {
     super.initState();
-    getCurrentLocation();
     initPolygons();
+    getCurrentLocation();
+    // initCurrentPolygonId();
   }
 
   @override
@@ -76,18 +78,6 @@ class _MapTestState extends State<MapTest> {
         markers: Set.of([_marker]),
         polygons: Set<Polygon>.of(
           polygons.values
-          //
-          // // for 문을 사용해 여러 개의 폴리곤 생성
-          // [
-          //   for (var i = 0; i < polygonData!.length; i++)
-          //     Polygon(
-          //       polygonId: PolygonId('polygon$i'),
-          //       points: polygonData![i],
-          //       fillColor: Colors.transparent,
-          //       strokeColor: Colors.red,
-          //       strokeWidth: 1,
-          //     )
-          // ],
         ),
       ),
     );
@@ -95,6 +85,8 @@ class _MapTestState extends State<MapTest> {
 
   void getCurrentLocation() async {
     currentLocation = await location.getLocation();
+
+    changePolygonColor();
     _initialPosition = CameraPosition(
       target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
       zoom: 14.4746,
@@ -113,8 +105,10 @@ class _MapTestState extends State<MapTest> {
       (newLoc) {
         currentLocation = newLoc;
         _updateMarker(newLoc);
+        changePolygonColor();
       },
     );
+
   }
 
   void _updateMarker(LocationData locationData) async {
@@ -132,4 +126,36 @@ class _MapTestState extends State<MapTest> {
     });
   }
 
+  void changePolygonColor() async {
+    for (var polygon in polygons.values) {
+      if(_isPointInPolygon(LatLng(currentLocation!.latitude!, currentLocation!.longitude!),polygon.points)){
+        print('로그');
+        currentPolygonId = polygon.polygonId.value;
+
+        Polygon tmp = polygon.clone();
+
+        setState(() {
+          polygons.remove(tmp.polygonId.value);
+
+          polygons[tmp.polygonId.value] = Polygon(
+              polygonId: PolygonId(tmp.polygonId.value),
+              points: tmp.points,
+              fillColor: Colors.orange,
+              strokeColor: Colors.red,
+              strokeWidth: 1
+          );
+        }); // Add this line to update the UI
+      }
+    }
+  }
+
+  bool _isPointInPolygon(LatLng point, List<LatLng> polygon) {
+    print('현재 위치');
+    print('${point.latitude} ${point.longitude}');
+
+    return point.latitude < polygon[0].latitude &&
+        point.latitude > polygon[2].latitude &&
+        point.longitude > polygon[0].longitude &&
+        point.longitude < polygon[2].longitude;
+  }
 }
